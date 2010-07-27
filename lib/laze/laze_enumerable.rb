98f56a6cd1b69e8600c82c656666d5e870eb6d79
Isaac <icambron@alum.mit.edu>
2010-07-26 22:29:13 -0400

    improved testing

---
+++ lib/laze/laze_enumerable.rb
@@ -0,0 +1,117 @@
+module Enumerable
+
+  def at_lazy(n)
+    self.each_with_index do |val, index|
+      return val if index == n
+    end
+    nil
+  end
+
+  def drop_lazy(n)
+    Enumerator.new do |yielder|
+      self.each_with_index do |val, index|
+        yielder << val if index >= n
+      end
+    end
+  end
+
+  def drop_while_lazy(&block)
+    Enumerator.new do |yielder|
+      found = false
+      self.each do |val|
+        next if block.(val) && !found
+        found = true
+        yielder << val
+      end
+    end
+  end
+
+  def grep_lazy(pattern, &block)
+    get_some do |yielder, val|
+      if pattern === val
+        yielder << (block_given? ? block.(val) : val)
+      end
+    end
+  end
+
+  def map_lazy(&block)
+    get_some do |yielder, val|
+      yielder << block.(val)
+    end
+  end
+  alias collect_lazy map_lazy
+
+  def reject_lazy(&block)
+    self.select_lazy{|val| !block.(val)}
+  end
+
+  def select_lazy(&block)
+    get_some do |yielder, val|
+      yielder << val if block.(val)
+    end
+  end
+  alias find_all_lazy select_lazy
+
+  def take_lazy(n)
+    Enumerator.new do |yielder|
+      self.each_with_index do |val, index|
+        break if index >= n
+        yielder << val
+      end
+    end
+  end
+
+  def take_while_lazy(&block)
+    Enumerator.new do |yielder|
+      self.each do |val|
+        break unless block.(val)
+        yielder << val
+      end
+    end
+  end
+
+  #this is disgusting, and the reason why Enumerator needs a has_next?
+  def zip_lazy(*args, &block)
+    rators = ([self] + args).map{|arr| arr.each}
+    Enumerator.new do |yielder|
+      loop do
+        answer = rators.map do |rator|
+          begin
+            [rator.better_next, true]
+          rescue StopIteration
+            [nil, false]
+          end
+        end
+        break if answer.none?{|val, signal| signal}
+        output = answer.map{|val, signal| val}
+        output = block.(output) if block_given?
+        yielder << output
+      end
+    end
+  end
+
+  private
+
+  def get_some
+    Enumerator.new do |yielder|
+      self.each do |value|
+        yield yielder, value
+      end
+    end
+  end
+end
+
+class Enumerator
+  def better_next
+    if @no_more
+      raise StopIteration
+    else
+      begin
+        self.next
+      rescue StopIteration
+        @no_more = true
+        raise StopIteration
+      end
+    end
+  end
+end
--- test/laze_test.rb
+++ test/laze_test.rb
@@ -1,22 +1,50 @@
+$LOAD_PATH.unshift File.dirname(__FILE__) + "/../lib"
+
 require "test/unit"
-require "lib/laze.rb"
+require "laze"
 
 class LazeTest < Test::Unit::TestCase
 
+  def test_at
+    assert_equal 17, Laze.naturals.at_lazy(16)
+  end
+
+  def test_drop
+    assert_equal 4, Laze.naturals.drop_lazy(3).first
+  end
+
+  def test_drop_while
+    assert_equal 5, Laze.naturals.drop_while_lazy{|i| i < 5}.first
+  end
+
+  def test_grep
+    assert_equal 'h', ('a'..'z').cycle.grep_lazy(/h/).first
+    assert_equal 'hello', ('a'..'z').cycle.grep_lazy(/h/){|a| a + 'ello'}.first
+  end
+
   def test_map
-    assert_equal (1..2).map_lazy{|a| a + 1}.to_a, [2,3]
+    assert_equal 2, Laze.naturals.map_lazy{|a| a + 1}.first
   end
 
-  def test_map_is_lazy
-    acc = []
-    (1..2).map_lazy{|a| acc << a; a}.first
-    assert_equal acc.count, 1
+  def test_reject
+    assert_equal 7, Laze.naturals.reject_lazy{|i| i % 2 == 0}.at_lazy(3)
   end
 
-  def test_lazy_map_chaining
-    acc = []
-    (1..20).map_lazy{|a| a + 1; acc << a}.map_lazy{|b| b*2}.first
-    assert_equal acc.count, 1
+  def test_select
+    assert_equal 8, Laze.naturals.select_lazy{|i| i % 2 == 0}.at_lazy(3)
+  end
+
+  def test_take
+    assert_equal [1, 2], Laze.naturals.take(2).to_a
+  end
+
+  def test_take_while
+    assert_equal [1, 2, 3], Laze.naturals.take(3).to_a
+  end
+
+  def test_zip_lazy
+    assert_equal [1, 1], Laze.naturals.zip_lazy(1..2).first
+    assert_equal [3, nil], Laze.naturals.zip_lazy(1..2).at_lazy(2)
   end
 
 end
